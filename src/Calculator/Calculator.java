8ddca934776774696387afc16aac215992a33b11
Hoja Arzanesh <ha1797@rit.edu>
2021-08-12 03:28:12 -0400

    fixed the case issue with equal sign on calculator not working.

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -41,10 +41,10 @@ public class Calculator {
 
         /* populate the precedence map */
         this.precedence = new HashMap<>();
-        this.precedence.put(MULTIPLY, 3);
-        this.precedence.put(DIVIDE, 3);
-        this.precedence.put(ADD, 2);
-        this.precedence.put(SUBTRACT, 2);
+        this.precedence.put( MULTIPLY, 3 );
+        this.precedence.put( DIVIDE, 3 );
+        this.precedence.put( ADD, 2 );
+        this.precedence.put( SUBTRACT, 2 );
     }
 
     /**
@@ -53,10 +53,10 @@ public class Calculator {
      *
      * @param tor the string (operator) passed in from view.
      */
-    public void Operator(String tor) {
+    public void Operator( String tor ) {
         // append current number inputted & operator to list.
-        this.tokens.add(this.text);
-        this.tokens.add(tor);
+        this.tokens.add( this.text );
+        this.tokens.add( tor );
 
         // clear "text"
         this.text = "";
@@ -67,7 +67,7 @@ public class Calculator {
      *
      * @param and the string (operand) passed in from view.
      */
-    public void Operand(String and) {
+    public void Operand( String and ) {
 
         // check if there was a percent or sign change.
         // if so, make text attribute equal to passed down string.
@@ -86,26 +86,29 @@ public class Calculator {
     /** calculates the total value of expressions in expressions attribute.*/
     public void equalSign() {
 
+        // add current text attribute string to tokens list.
+        this.tokens.add( this.text );
+
         // make queue for postfix and stack for operators.
         Queue<String> postfix = new LinkedList<>();
         Stack<String> opStack = new Stack<>();
 
         // transform tokens into postfix notation.
-        intoPostfix(postfix, opStack);
+        intoPostfix( postfix, opStack );
 
         // loop through the postfix queue, and put each token into an ArrayList.
         List<String> elements = new ArrayList<>();
-        while (!postfix.isEmpty()) {
-            elements.add(postfix.remove());
+        while ( !postfix.isEmpty() ) {
+            elements.add( postfix.remove() );
         }
 
         // loop through elements list until encounter an operator,
         // then do that operation with previous two numbers.
         Stack<Double> savedNumbers = new Stack<>(); // saved numbers
-        for (String element : elements) {
+        for ( String element : elements ) {
 
             // token at index is an operator
-            if (isOperator(element)) {
+            if ( isOperator( element ) ) {
 
                 // save operands.
                 double b = savedNumbers.pop();
@@ -113,20 +116,16 @@ public class Calculator {
 
                 // do operation.
                 switch (element) {
-                    case ("+"):
-                        savedNumbers.push(a + b);
-                    case ("-"):
-                        savedNumbers.push(a - b);
-                    case ("*"):
-                        savedNumbers.push(a * b);
-                    case ("/"):
-                        savedNumbers.push(a / b);
+                    case ("+") -> savedNumbers.push(a + b);
+                    case ("-") -> savedNumbers.push(a - b);
+                    case ("*") -> savedNumbers.push(a * b);
+                    case ("/") -> savedNumbers.push(a / b);
                 }
             }
 
             // token is a number
             else {
-                savedNumbers.push(Double.parseDouble(element));
+                savedNumbers.push( Double.parseDouble( element ) );
             }
         }
 
@@ -199,41 +198,34 @@ public class Calculator {
         for (String token : this.tokens) {
 
             // token is not an operator (it is numeric), so we put it in postfix.
-            if (!isOperator(token)) {
-                postfix.add(token);
-                continue;
+            if ( !isOperator( token ) ) {
+                postfix.add( token );
             }
 
             // token is an operator, and opStack is empty, so we put it in opStack.
-            if (isOperator(token) && opStack.isEmpty()) {
-                opStack.add(token);
-                continue;
+            else if ( isOperator( token ) && opStack.isEmpty() ) {
+                opStack.add( token );
             }
 
             // token is an operator, and opStack is not empty, so check precedence.
-            if (isOperator(token) && !opStack.isEmpty()) {
+            else if ( isOperator( token ) && !opStack.isEmpty() ) {
 
                 // precedence of token is higher than or equal to precedence of top of opStack.
-                if (greaterEqualPrecedence(token, opStack.peek())) {
-                    opStack.push(token);
-                }
-
-                // precedence of token is less than precedence of top of stack.
-                else {
+                if ( !greaterEqualPrecedence( token, opStack.peek() ) ) {
 
                     // loop until token has greater or equal precedence to top.
-                    while (!greaterEqualPrecedence(token, opStack.peek())) {
-                        postfix.add(opStack.pop());
+                    while ( !greaterEqualPrecedence( token, opStack.peek() ) ) {
+                        postfix.add( opStack.pop() );
                     }
                     // push token to opStack.
-                    opStack.push(token);
                 }
+                opStack.push(token);
             }
         }
 
         // add remaining tokens in opStack to postfix.
-        while (!opStack.isEmpty()) {
-            postfix.add(opStack.pop());
+        while ( !opStack.isEmpty() ) {
+            postfix.add( opStack.pop() );
         }
     }
 
@@ -243,8 +235,8 @@ public class Calculator {
      * @param token token of from token list.
      * @return true if token is an operator, false if not.
      */
-    private boolean isOperator(String token) { //
-        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
+    private boolean isOperator( String token ) { //
+        return token.equals( "+" ) || token.equals( "-" ) || token.equals( "*" ) || token.equals( "/" );
     }
 
     /**
@@ -253,8 +245,8 @@ public class Calculator {
      * @param top top of opStack.
      * @return true if token has greater or equal precedence, false if token has less precedence than top.
      */
-    private boolean greaterEqualPrecedence(String token, String top) {
-        return this.precedence.get(token) >= this.precedence.get(top);
+    private boolean greaterEqualPrecedence( String token, String top ) {
+        return this.precedence.get( token ) >= this.precedence.get( top );
     }
 
     /**
@@ -262,8 +254,8 @@ public class Calculator {
      *
      * @param observer the view
      */
-    public void addObserver(Observer< Calculator > observer) {
-        this.observers.add(observer);
+    public void addObserver( Observer< Calculator > observer ) {
+        this.observers.add( observer );
     }
 
     /**
@@ -271,8 +263,8 @@ public class Calculator {
      * the update method
      */
     private void notifyObservers() {
-        for (Observer< Calculator > obs : this.observers) {
-            obs.update(this);
+        for ( Observer< Calculator > obs : this.observers ) {
+            obs.update( this );
         }
     }
 
